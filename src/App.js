5af9ddb6d5180a1ffb2872bcd3c4f2bfe67556e5
satish <satishdaliboina@gmail.com>
2024-07-19 21:24:23 +0530

    first comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./components/signup";
+import Login from "./components/login";
+import AddNotes from "./components/addnote";
+import Notes from "./components/notes";
+import ViewNote from "./components/viewnotes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/notes" element={<Notes />} />
+        <Route path="/add-note" element={<AddNotes />} />
+        <Route path="/notes/:id" element={<ViewNote />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
